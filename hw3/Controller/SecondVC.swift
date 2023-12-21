3275d857eef586034042744b61f4edde8a6c2c20
Scarbete <megavertal@gmail.com>
2023-12-21 22:59:20 +0600

    Initial Commit

---
+++ hw3/Controller/SecondVC.swift
@@ -0,0 +1,67 @@
+import UIKit
+import SnapKit
+
+
+
+class SecondVC: UIViewController, UITableViewDataSource {
+    private let cell = "Quasar"
+    private let tableView = UITableView()
+    
+    private let peoples: [Contact] = [
+        .init(name: "Jane Cooper", phoneNumber: "(270) 555-0117", image: "userImage"),
+        .init(name: "Devon Lane", phoneNumber: "(308) 555-0121", image: "userImage"),
+        .init(name: "Darrell Steward", phoneNumber: "(684) 555-0102", image: "userImage"),
+        .init(name: "Devon Lane", phoneNumber: "(704) 555-0127", image: "userImage"),
+        .init(name: "Courtney Henry", phoneNumber: "(505) 555-0125", image: "userImage"),
+        .init(name: "Wade Warren", phoneNumber: "(225) 555-0118", image: "userImage"),
+        .init(name: "Bessie Cooper", phoneNumber: "(406) 555-0120", image: "userImage"),
+        .init(name: "Robert Fox", phoneNumber: "(480) 555-0103", image: "userImage"),
+        .init(name: "Jacob Jones", phoneNumber: "(702) 555-0122", image: "userImage"),
+        .init(name: "Jenny Wilson", phoneNumber: "(239) 555-0108", image: "userImage")
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+    }
+    
+    
+    private func initUI() {
+        view.backgroundColor = .white
+        tableView.dataSource = self
+    
+        view.safeAreaLayoutGuide.owningView?.backgroundColor = UIColor(hex: "#F6F6F6")
+        navigationItem.title = "My Contacts"
+        
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+
+extension SecondVC {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        peoples.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cell)
+        cell.textLabel?.text = peoples[indexPath.row].name
+        cell.textLabel?.font = .systemFont(ofSize: 17)
+        cell.imageView?.image = UIImage(named: peoples[indexPath.row].image)
+        cell.detailTextLabel?.text = peoples[indexPath.row].phoneNumber
+        cell.detailTextLabel?.textColor = UIColor(hex: "#8A8A8D")
+        return cell
+    }
+}
---
+++ hw3/Controller/ViewController.swift
@@ -0,0 +1,155 @@
+import UIKit
+import SnapKit
+
+
+
+class ViewController: UIViewController {
+    
+    private lazy var loginLabel: UILabel = MakerView.makerView.createLabel(
+        text: "Login", fontSize: 18, fontWeight: .bold
+    )
+    
+    private lazy var loginImage: UIImageView = MakerView.makerView.createImage(
+        image: UIImage(systemName: "person.crop.circle")
+    )
+    
+    private lazy var welcomeBackLabel: UILabel = MakerView.makerView.createLabel(
+        text: "Welcome back , Rohit thakur", textColor: UIColor(hex: "#5B5B5B")
+    )
+    
+    private lazy var mainImage: UIImageView = MakerView.makerView.createImage(
+        image: UIImage(named: "mainImage")
+    )
+    
+    private lazy var orangeLabel: UILabel = MakerView.makerView.createLabel(
+        text: "Enter Your Mobile Number", textColor: UIColor(hex: "#F2796B"), fontSize: 17, fontWeight: .bold
+    )
+    
+    private lazy var numberTF: UITextField = MakerView.makerView.createTF(
+        placeholder: "Enter phone number", backgroundColor: .white, cornerRadius: 10, leftView: true, rightView: true
+    )
+    
+    private lazy var loginButton: UIButton = MakerView.makerView.createButton(
+        text: "Login", textColor: .white, fontSize: 15, fontWeight: .bold, cornerRadius: 10, backgroundColor: UIColor(hex: "#F2796B")
+    )
+
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
+    }
+    
+
+    private func initUI() {
+        view.backgroundColor = UIColor(hex: "#F6F6F6")
+        numberTF.delegate = self
+        numberTF.keyboardType = .numberPad
+        configureLoginLabel()
+        configureLoginImage()
+        configureWelcomeBackLabel()
+        configureMainImage()
+        configureOrangeLabel()
+        configureNumberTF()
+        configureLoginButton()
+    }
+
+    private func configureLoginLabel() {
+        view.addSubview(loginLabel)
+        
+        loginLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.leading.equalToSuperview().offset(24)
+        }
+    }
+
+    private func configureLoginImage() {
+        view.addSubview(loginImage)
+        
+        loginImage.snp.makeConstraints { make in
+            make.top.equalTo(loginLabel.snp.top)
+            make.leading.equalTo(loginLabel.snp.trailing).offset(4)
+        }
+    }
+
+    private func configureWelcomeBackLabel() {
+        view.addSubview(welcomeBackLabel)
+        
+        welcomeBackLabel.snp.makeConstraints { make in
+            make.top.equalTo(loginLabel.snp.bottom).offset(4)
+            make.leading.equalToSuperview().offset(24)
+        }
+    }
+
+    private func configureMainImage() {
+        view.addSubview(mainImage)
+        
+        mainImage.snp.makeConstraints { make in
+            make.top.equalTo(welcomeBackLabel.snp.bottom).offset(49)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(231)
+        }
+    }
+
+    private func configureOrangeLabel() {
+        view.addSubview(orangeLabel)
+        
+        orangeLabel.snp.makeConstraints { make in
+            make.top.equalTo(mainImage.snp.bottom).offset(15)
+            make.centerX.equalToSuperview()
+        }
+    }
+
+    private func configureNumberTF() {
+        view.addSubview(numberTF)
+        
+        numberTF.snp.makeConstraints { make in
+            make.top.equalTo(orangeLabel.snp.bottom).offset(31)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(43)
+        }
+    }
+
+    private func configureLoginButton() {
+        view.addSubview(loginButton)
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(numberTF.snp.bottom).offset(64)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.height.equalTo(43)
+        }
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonTapped(_ sender: UIButton) {
+        if numberTF.hasText, let text = numberTF.text {
+            if text.hasPrefix("996") && text.count == 12 {
+                numberTF.text = ""
+                numberTF.placeholder = "Enter Number"
+                numberTF.layer.borderWidth = 0
+                numberTF.layer.borderColor = .none
+                navigationController?.pushViewController(SecondVC(), animated: true)
+            }
+            else {
+                numberTF.text = ""
+                numberTF.placeholder = "Номер должен содержать 996 и 12 символов!"
+                numberTF.layer.borderWidth = 1
+                numberTF.layer.borderColor = UIColor.red.cgColor
+            }
+        }
+        else {
+            numberTF.placeholder = "Заполните пожалуйста!"
+        }
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textField(_ sender: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
+    
+}
